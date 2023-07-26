9ec0ecf16217e369ef726b8b50630ebf5d0090c6
BigShuff <stevedavis0504@gmail.com>
2023-07-26 17:25:59 +0100

    all changes late

--- resources/js/bingo.js
+++ resources/js/bingo.js
@@ -1,6 +1,5 @@
 const ballGame = {
     ballSet : [],
-    checkDuplicate : [],
     newCard : [],
     
 
@@ -12,48 +11,24 @@ const ballGame = {
     },
 
     getNewCards(){
-        this.checkDuplicate.length = 0;
-        this.newCard.length = 0;
-       
-            for (let i = 0; i <= 14; i++){
-                let randNum = Math.floor((Math.random() * 90) + 1);
-                // I need to check for duplicate numbers
-                if (i = 1){
-                    this.newCard.push(randNum);
-                    } else {
-                    this.checkDuplicate.push(randNum);
-                    }
-
-
-                
-                /*
-                for (let i = 1; i <= this.newCard.length; i++){
-                    if (this.newCard[i - 1] === this.checkDuplicate[i - 1]){
-                        console.log('Their was a duplicate');
-                        this.newCard.splice(i);
-                        this.checkDuplicate.splice(i);
-                    }
-                }*/
-
-            }
-            
-    },    
-           
-
-
+        for (i = 1; i <= 15; i++){
+        let randNum = Math.floor((Math.random() * this.ballSet.length) + 1);
+        this.newCard.push(randNum);
+        delete this.ballSet[randNum -1];
+        }
+    },
 };
 
 ballGame.replaceBalls();
 ballGame.getNewCards();
 let set = ballGame.ballSet;
-let cardCheck = ballGame.checkDuplicate;
 let card = ballGame.newCard;
-console.log(set.join(' '));
-console.log(cardCheck.join(', '))
-console.log(card.join(', '));
+// console.log(set.join(' '));
+// console.log(cardCheck.join(', '))
+// console.log(card.join(', '));
 
 
-//document.getElementById("ballSet"). innerHTML = set.join(' ');
-//document.getElementById("cardOne"). innerHTML = cardOne.join(' ');
+document.getElementById("ballSet"). innerHTML = set.join(' ');
+document.getElementById("player"). innerHTML = card.join(' ');
 //document.getElementById("cardTwo"). innerHTML = cardTwo.join(' ');
 //document.getElementById("cardThree"). innerHTML = cardThree.join(' ');
