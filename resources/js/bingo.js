99919caa766ceb02847326e2123d397563f576b2
BigShuff <stevedavis0504@gmail.com>
2023-07-31 12:21:01 +0100

    latest update 31

--- resources/js/bingo.js
+++ resources/js/bingo.js
@@ -1,54 +1,77 @@
 const ballGame = {
-    ballSet : [],
-    newCard : [],
-    
+    ballSet: [],
+    newCard: [],
 
 
-    replaceBalls(){
-        for (let i = 1; i <= 90; i++){
+
+    replaceBalls() {
+        for (let i = 1; i <= 90; i++) {
             this.ballSet.push([i]);
         }
     },
 
-    getNewCards(){
-       
-
+    getNewCards() {
+        let numberToBeChecked;
+        let tempArr = [];
         //generate 15 random numbers for card
-        for (let i = 0; i <= 14; i++){
+        //for (let i = 0; i <= 14; i++) {
+        while (this.newCard.length <= 14) {
             let randNum = Math.floor((Math.random() * this.ballSet.length) + 1);
-            this.newCard.push(randNum)
-        }
-        //compare each single number in newCard array. 
-        for(let i = 0; i <= this.newCard.length -1; i++){
-        //     //with the rest of the numbers in the array to find duplicates
-            let IndexToBeChecked = this.newCard[i];
-            
-             for(let j = 0; j <= this.newCard.length -1; j++){
-                 if (this.newCard[j] == IndexToBeChecked){
-                   //console.log('this will be the same index so the same number');
-                    continue;
-                 } else if (this.newCard[j] != IndexToBeChecked){
-                    if (this.newCard[j] == IndexToBeChecked){
-                        console.log('this is a duplicate')
-                    }
-                 }
-                 //console.log('this plus' + numberToBeChecked);
-             }
-         }
-        //insert into an array at a specific place
-        //about inserting into an array
-        //array: name of array / start: the element you wish to inset at (the changes take place immediately after)
-        //deleteCount: the amount of items you wish to delete starting place is start index,
-        //item 1 etc: the content of what you wish to put into the array at start index.
-        //array.splice(start, deleteCount, item 1, item2);
-        //this.ballSet.splice(3, 1, 6); takes out number 4 and puts 6 in it's place.
 
-        
-        
-        
-        },
-    
+            if (this.newCard.indexOf === 0) {
+                this.newCard.push(randNum);
+            } else {
+                for (i = 1; i <= this.newCard.length; i++) { 
+                    if (randNum === this.newCard[i]){
+                        continue;
+                    } else {
+                        this.newCard.push(randNum);
+                    }
+                }
+            }
+
+
+
+
+
+
+            this.newCard.push(randNum);
+            console.log(this.newCard);
+        }
+    },
 };
+
+
+//take each number of the array in order and check it against the rest of the numbers to find duplicates
+// for (let i = 0; i <= this.newCard.length - 1; i++) {
+//     for (let j = 0; j <= this.newCard.length - 1; j++) {
+//         if (this.newCard.indexOf(this.newCard[i]) === this.newCard.indexOf(this.newCard[j])) {
+//             continue;
+//         } else {
+//             tempArr.push(this.newCard[j]);
+//             }
+//     }
+
+// }
+//console.log(tempArr);
+
+
+
+
+
+//insert into an array at a specific place
+//about inserting into an array
+//array: name of array / start: the element you wish to inset at (the changes take place immediately after)
+//deleteCount: the amount of items you wish to delete starting place is start index,
+//item 1 etc: the content of what you wish to put into the array at start index.
+//array.splice(start, deleteCount, item 1, item2);
+//this.ballSet.splice(3, 1, 6); takes out number 4 and puts 6 in it's place.
+
+
+
+
+
+
 
 ballGame.replaceBalls();
 ballGame.getNewCards();
