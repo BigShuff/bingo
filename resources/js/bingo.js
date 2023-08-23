bb2925a2c28d1a9f6d2db3589b2d35ba3af7fa83
BigShuff <stevedavis0504@gmail.com>
2023-08-23 18:08:42 +0100

    player 2 finished

--- resources/js/bingo.js
+++ resources/js/bingo.js
@@ -1,15 +1,51 @@
 const bingoGame = {
   ballSet: [],
- 
+
   replaceBalls() {
     for (let i = 1; i <= 90; i++) {
       this.ballSet.push([i]);
     }
   },
 
-  displayBalls(){
+  displayBalls() {
     let set = bingoGame.ballSet;
     document.querySelector(".fullNumSet").innerHTML = set.join(' ');
+  },
+
+  displayBingoCards(playerNum) {
+    if (playerNum === '1' || playerNum === "2") {
+      //set out card for computer
+      //get the template I want to use
+      const temp1 = document.querySelector(".templateForOne").content;
+      //make a copy of the template that I can change 
+      //let copy1 = temp1.content.cloneNode(true);
+      const copy1 = document.importNode(temp1, true);
+      //same as above
+      const temp2 = document.querySelector(".templateForOne").content;
+      const copy2 = document.importNode(temp2, true);
+      //after this the template could be appended using 
+      //document.querySelector(".htmlElement").appendChild(copy1);
+      //but this time I want to change the playerLabel to represent computer and player 1
+      if (playerNum === "1") {
+        const element = copy1.querySelector(".playerLabel");
+        element.textContent = "Computer";
+        //set out card for player 1
+        const element2 = copy2.querySelector(".playerLabel");
+        element2.textContent = "Player 1";
+      } 
+      if(playerNum === "2") {
+        const element = copy1.querySelector(".playerLabel");
+        element.textContent = "Player 1";
+        //set out card for player 1
+        const element2 = copy2.querySelector(".playerLabel");
+        element2.textContent = "Player 2";
+      }
+      
+
+      document.querySelector(".divForPlayer1").appendChild(copy1);
+      document.querySelector(".divForPlayer2").appendChild(copy2);
+
+    }
   },
 
   //   getNewCards() {
@@ -50,7 +86,8 @@ closeModal.addEventListener("click", () => {
   modal.close();
   bingoGame.replaceBalls();
   bingoGame.displayBalls();
-  
+  bingoGame.displayBingoCards(playerNum);
+
 });
 
 
